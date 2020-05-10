e78b93c8171268c034fc107f105208407f323eda
David Kershaw <dk107dk@hotmail.com>
2020-05-10 00:22:40 -0400

    improved config class

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cdocs",
-    version="0.0.3",
+    version="0.0.4",
     author="David Kershaw",
     author_email="dk107dk@hotmail.com",
     description="Cdocs is a super simple contextual help library",
