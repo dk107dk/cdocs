9c7113e837253898395a13f76488cd91e3b73b8a
David Kershaw <dk107dk@hotmail.com>
2020-06-21 17:57:49 -0400

    added formats to metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cdocs",
-    version="0.0.25",
+    version="0.0.26",
     author="David Kershaw",
     author_email="dk107dk@hotmail.com",
     description="Cdocs is a super simple contextual help library",
