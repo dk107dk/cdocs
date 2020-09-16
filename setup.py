a0e4e66dc1ccb52534c93a637f3cb1eae3916d42
David Kershaw <dk107dk@hotmail.com>
2020-09-15 21:27:47 -0400

    fixed bug: . at beginning of root name no longer interpreted as extension

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cdocs",
-    version="0.0.37",
+    version="0.0.38",
     author="David Kershaw",
     author_email="dk107dk@hotmail.com",
     description="Cdocs is a super simple contextual help library",
